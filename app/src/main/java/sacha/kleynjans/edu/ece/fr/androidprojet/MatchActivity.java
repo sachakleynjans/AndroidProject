83a4e8976622441713455cfb4e4650dece0cea81
Sacha Kleynjans <sacha.kleynjans@gmail.com>
2017-04-07 19:48:48 +0200

    Changement de fragment

--- app/src/main/java/sacha/kleynjans/edu/ece/fr/androidprojet/MatchActivity.java
+++ app/src/main/java/sacha/kleynjans/edu/ece/fr/androidprojet/MatchActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 
 /**
@@ -20,13 +21,13 @@ public class  MatchActivity extends FragmentActivity {
         fragmentTransaction.add(R.id.container, fragment);
         fragmentTransaction.commit();
     }
-    protected void goStartMatch(){
+    protected void goStartMatch(View v){
+        setContentView(R.layout.match_activity);
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         MatchCounterFragment fragment = new MatchCounterFragment();
         fragmentTransaction.replace(R.id.container, fragment);
         fragmentTransaction.commit();
-
     }
 }
 
